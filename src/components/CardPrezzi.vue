97dabb98d91d088d5fd034719f735601312fa379
italocirillo <italocirillo@gmail.com>
2023-04-27 18:22:54 +0200

    SEZIONE PREZZI

--- src/components/CardPrezzi.vue
+++ src/components/CardPrezzi.vue
@@ -2,16 +2,22 @@
 import "@fortawesome/fontawesome-free/css/all.css";
 export default {
     name: "CardPrezzi",
+    props: {
+        pianoAcquisto: String,
+        prezzo: String,
+        descrizione: String,
+        nCard: Number,
+    }
 }
 </script>
 
 <template>
     <div class="card-prezzi">
-        <div class="fascia"><span>best</span></div>
-        <span class="etichetta">Basic</span>
-        <h2>$9.00</h2>
+        <div v-if="nCard === 2" class="fascia"><span>best</span></div>
+        <span class="etichetta">{{ pianoAcquisto }}</span>
+        <h2>{{ prezzo }}</h2>
         <p>Per Month</p>
-        <p>All the basic for business that are just getting started</p>
+        <p>{{ descrizione }}</p>
         <hr>
         <ul>
             <li><i class="fa-regular fa-circle-check"></i>Full Access</li>
@@ -39,24 +45,24 @@ export default {
     position: relative;
 
     .fascia {
-        width: 100px;
-        height: 100px;
-        // background-color: red;
+        width: 65px;
+        height: 65px;
         overflow: auto;
         position: absolute;
-        top: -10px;
-        right: -10px;
+        top: -5px;
+        right: -5px;
 
         span {
             position: absolute;
             display: block;
-            width: 170px;
-            padding: 1px 0;
+            width: 100px;
             background-color: #f17426;
-            left: -25px;
-            top: 30px;
+            left: -12px;
+            top: 20px;
             transform: rotate(45deg);
             color: #fff;
+            font-weight: 900;
+            font-size: 0.6rem;
             text-transform: uppercase;
             text-align: center;
         }
@@ -69,16 +75,16 @@ export default {
             z-index: -1;
             content: '';
             display: block;
-            border: 5px solid #f17426;
+            border: 2px solid #b1551c;
         }
 
         &::before {
             top: 0;
-            left: 0;
+            left: 0px;
         }
 
         &::after {
-            bottom: 0;
+            bottom: 0px;
             right: 0;
         }
     }
--- src/components/SezionePrezzi.vue
+++ src/components/SezionePrezzi.vue
@@ -13,13 +13,16 @@ export default {
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <CardPrezzi />
+                    <CardPrezzi :pianoAcquisto="'Basic'" :prezzo="'9.00$'"
+                        :descrizione="'All the basic for business that are just getting started'" :nCard="1" />
                 </div>
                 <div class="col">
-                    <CardPrezzi />
+                    <CardPrezzi :pianoAcquisto="'Business'" :prezzo="'39.00$'"
+                        :descrizione="'Better for growing businesses that want more customers'" :nCard="2" />
                 </div>
                 <div class="col">
-                    <CardPrezzi />
+                    <CardPrezzi :pianoAcquisto="'Enterprise'" :prezzo="'79.00$'"
+                        :descrizione="'Advanced features for pros who need more customization'" :nCard="3" />
                 </div>
             </div>
         </div>
